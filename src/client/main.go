fed542ba5360d20a1fe3388f9ec00eaccdb468b5
Raniel <rnldourado@gmail.com>
2024-09-16 22:49:17 -0300

    fix: changing args to CLI

--- src/client/main.go
+++ src/client/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -9,24 +8,56 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	args := os.Args
 
-	fmt.Print("Digite o IP do server: ")
-	serverURI, _ := reader.ReadString('\n')
+	if len(args) < 2 {
+		help()
+		return
+	}
+
+	var conn *net.Conn
+
+	if (args[1] == "help"){
+		help()
+	}else if (args[1] == "join" && args[3] == "search"){
+		conn = join(args[2])
+		fmt.Println(search(args[2]))
+	}else{
+		help()
+	}
+
+	(*conn).Close()
+}
+
+func help() {
+	fmt.Println("Uso: go run main.go join [serverip] seach [file hash]")
+	return
+}
+
+func join(serverURI string) *net.Conn {
+	// reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Digite o IP do server: ")
+	// serverURI, _ := reader.ReadString('\n')
 
 	conn, err := net.Dial("tcp", strings.Trim(serverURI, "\n"))
 
 	if err != nil {
 		fmt.Println("Erro ao conectar ao servidor:", err)
-		return
+		return nil
 	}
-	defer conn.Close()
 
-	fmt.Print("Digite sua mensagem: ")
-	text, _ := reader.ReadString('\n')
+	return &conn
 
-	fmt.Fprintf(conn, text)
+	// fmt.Print("Digite sua mensagem: ")
+	// text, _ := reader.ReadString('\n')
+	//
+	// fmt.Fprintf(conn, text)
+	//
+	// message, _ := bufio.NewReader(conn).ReadString('\n')
+	// fmt.Print("Resposta do servidor: " + message)
+}
 
-	message, _ := bufio.NewReader(conn).ReadString('\n')
-	fmt.Print("Resposta do servidor: " + message)
+func search(hash string) []byte {
+	return []byte(hash)
 }
